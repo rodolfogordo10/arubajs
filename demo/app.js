86604354adb47f6e4f5b5daac52deb2c79c7ebc4
vinnylinck <vinnylinck@outlook.com>
2015-05-25 12:03:02 -0300

    Fixed  - #20

--- demo/app.js
+++ demo/app.js
@@ -1,7 +1,17 @@
-/*global angular, console*/
+/*global angular, console, setTimeout*/
 angular.module('demoApp', ['aruba.js']);
+
 angular.module('demoApp').run(['$log', 'boot.options', function ($log, options) {
     'use strict';
     console.log('demoApp has been started...', options.custom_key);
     console.info("Let's go to Aruba? ", $log.dispatch());
+}]);
+
+angular.module('demoApp').controller('MainController', ['$scope', function ($scope) {
+
+    $scope.$on('ARUBAJS_INTERNAL_ERROR', function () {
+        console.warn('Error catched successfully!');
+    });
+
+    throw "Teste error handling!";
 }]);
--- src/error-handler.js
+++ src/error-handler.js
@@ -3,15 +3,29 @@
     "use strict";
 
     // defining behaviour
-    function ArubaExceptionHandler() {
+    function ArubaExceptionHandler($injector) {
 
         return function (exception, cause) {
+
+            // preparing message to be dispatched
+            var dispatcher = null,
+                data = {
+                    "error": exception,
+                    "details": cause
+                };
+
             // need to figure out what to do with exceptions
-            console.error(exception);
-            console.debug(cause);
-            throw exception;
+            console.error('[ARUBAJS_INTERNAL_ERROR]', exception);
+            console.debug('[ARUBAJS_DEBUG_INFO]', cause);
+
+            // dispatching message
+            dispatcher = $injector.get('$rootScope');
+            dispatcher.$broadcast('ARUBAJS_INTERNAL_ERROR', data);
         };
     }
+
+    // injecting
+    ArubaExceptionHandler.$inject = ['$injector'];
 
     // registering on angular
     angular
