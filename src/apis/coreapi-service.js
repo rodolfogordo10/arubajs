ad25b57f0c63efb6b709830c6e7f735180b530fd
felipekm <felipe.kautzmann@gmail.com>
2015-08-21 14:37:04 -0300

    Ajuste no UpperCase()

--- src/apis/coreapi-service.js
+++ src/apis/coreapi-service.js
@@ -5,7 +5,8 @@
     angular.module('aruba.js').service('coreApiService', ['boot.options', function (options) {
 
         var base_core_url = options.core_url + options.def_api_version + "/" + options.def_api_app + '/',
-            accounts_url  = options.accounts_url;
+            accounts_url  = options.accounts_url,
+            application_name = options.def_api_app;
 
         this.getCoreUrl = function () {
             return base_core_url;
@@ -13,6 +14,10 @@
 
         this.getAccountsUrl = function () {
             return accounts_url;
+        };
+
+        this.getApplicationName = function () {
+            return application_name.charAt(0).toUpperCase() + application_name.slice(1);
         };
 
     }]);
