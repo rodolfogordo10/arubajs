742f3f7bc46bfe0807617c4afaab0a47e2ee2553
felipekm <felipe.kautzmann@gmail.com>
2015-08-21 14:00:48 -0300

    Desenvolver função que retorne nome da aplicação #39

--- src/apis/coreapi-service.js
+++ src/apis/coreapi-service.js
@@ -5,14 +5,22 @@
     angular.module('aruba.js').service('coreApiService', ['boot.options', function (options) {
 
         var base_core_url = options.core_url + options.def_api_version + "/" + options.def_api_app + '/',
-            accounts_url  = options.accounts_url;
+            accounts_url  = options.accounts_url,
+            app_name      = options.def_api_app;
 
+        // Returns Core API Service URL
         this.getCoreUrl = function () {
             return base_core_url;
         };
 
+        // Returns Accounts URL
         this.getAccountsUrl = function () {
             return accounts_url;
+        };
+
+        // Returns Application name capitalizing the first letter with UpperCase
+        this.getAppplicationName = function () {
+            return app_name.charAt(0).toUppercase() + app_name.slice(1);
         };
 
     }]);
